6dbf7beac6cf5ed3016ee2f9a5b86e998dc25c4c
GioGioCa <darkconor2500@hotmail.com>
2024-03-04 23:39:43 -0600

    Feat(Screen): Agregadas funciones de Geolocator

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -1,14 +1,48 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:geolocator/geolocator.dart';
 
 const MAPBOX_ACCESS_TOKEN =
     'pk.eyJ1Ijoiem9kaWFyayIsImEiOiJjbHRkdDU4ZWQwNzB1MmpwN21hZGdiN25rIn0.vha1owhjdzEK5d4YwPT8iA';
 
-final myPosition = LatLng(latitude, longitude);
+// final myPosition = LatLng(latitude, longitude);
 
-class MapScreen extends StatelessWidget {
+class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
+
+  @override
+  State<MapScreen> createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  LatLng? myPosition;
+
+  Future<Position> determinePosition() async {
+    LocationPermission permission;
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('error');
+      }
+    }
+    return await Geolocator.getCurrentPosition();
+  }
+
+  void getCurrentLocation() async {
+    Position position = await determinePosition();
+    setState(() {
+      myPosition = LatLng(position.latitude, position.longitude);
+      print(myPosition);
+    });
+  }
+
+  @override
+  void initState() {
+    getCurrentLocation();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,29 +51,28 @@ class MapScreen extends StatelessWidget {
         centerTitle: true,
         title: const Text('Map'),
       ),
-      body: FlutterMap(
-        options: MapOptions(
-          initialCenter: myPosition,
-          minZoom: 5,
-          maxZoom: 25,
-          initialZoom: 18,
-        ),
-        children: [
-          TileLayer(
-            urlTemplate:
-                'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}',
+      body:  myPosition == null
+          ? const CircularProgressIndicator()
+          : FlutterMap(
+            options: MapOptions(
+              center: myPosition,
+              minZoom: 5,
+              maxZoom: 25,
+              initialZoom: 18,
+            ),
+            children: [
+              TileLayer(
+                urlTemplate:
+                    'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}',
                 additionalOptions: const {
                   'accessToken': MAPBOX_ACCESS_TOKEN,
                   'id': 'mapbox/navigation-night-v1',
                 },
-          ),
-          MarkerLayer(
-            markers: [
-              Marker(
-                point: myPosition, child: const FlutterLogo()),
-            ]
-            ),
-        ],
+              ),
+              MarkerLayer(markers: [
+                Marker(point: myPosition!, child: const FlutterLogo()),
+              ]),
+            ],
       ),
     );
   }
